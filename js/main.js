1d1979b81eb9efe4bc3f514acb8de69129b67939
hkobulov <kobulov.2000@mail.ru>
2021-09-17 15:51:20 +0500

    complete

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 var elForm = document.querySelector('.form');
 var elInput = document.querySelector('.form__input');
 
-var elFormAll = document.querySelector('.form-list__item__area')
+var elFormAll = document.querySelectorAll('.form-list__item__area')
 var elFormWalk = document.querySelector('.cal-walk');
 var elFormVelo = document.querySelector('.cal-velo');
 var elFormCar = document.querySelector('.cal-car');
 var elFormAir = document.querySelector('.cal-air');
+var elFormAlert = document.querySelector('.form__alert');
 
 // SPEEDS
 var speedWalk = 3.6;
@@ -19,25 +20,36 @@ function calculateTimeSpeed (number, speed){
     var resultFullMin = ((number / speed) - resultHour) * 60;
     var resultSec = Math.floor((resultFullMin - resultMin) *60);
     
-
-    if (resultHour > 0 && resultMin > 0 && resultSec > 0){
-        return `${resultHour} ${resultMin} ${resultSec}`
-    }
-    // return `${resultHour} soat, ${resultMin} daqiqa, ${resultSec} soniya`
+    return `${resultHour} soat, ${resultMin} daqiqa, ${resultSec} soniya`;
 }
+
 
 function checkResult(evt){
     evt.preventDefault();
+    
+    var userNumber = elInput.value-0;
 
-    var userNumber = elInput.value;
-
+  
+    
+    if (userNumber===0 || userNumber<0){
+        elFormAlert.textContent = 'Iltimos, musbat son kiriting!';
+        elFormAlert.classList.add('form__alert-design');
+        elInput.value = null;
+        return;
+    } else if (isNaN(userNumber)) {
+        elFormAlert.textContent = 'Iltimos, raqam kiriting!';
+        elFormAlert.classList.add('form__alert-design');
+        return;
+    } else {
+        elFormAlert.textContent = null;
+        elFormAlert.classList.remove('form__alert-design');
+    }
+    
     elFormWalk.textContent = calculateTimeSpeed(userNumber, speedWalk);
     elFormVelo.textContent = calculateTimeSpeed(userNumber, speedVelo);
     elFormCar.textContent = calculateTimeSpeed(userNumber, speedCar);
     elFormAir.textContent = calculateTimeSpeed(userNumber, speedAir);
 }
-
-
 
 elForm.addEventListener('submit', checkResult);
 
